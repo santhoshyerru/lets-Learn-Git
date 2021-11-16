e5785e47e82c49f4e50a010c02dc408e0acbefde
santhosh yerru <santhoshnaidu345@gmail.com>
2021-11-16 22:09:15 +0530

    removing space in message

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ import java.util.*;
 public class sum{
     public static void main(String args[]){
         int a=0,b=1;
-        System.out.println("sum = "+(a+b));
+        System.out.println("sum ="+(a+b));
     }
 }
